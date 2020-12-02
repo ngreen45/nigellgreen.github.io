33f76d9141f4abdec5105062e0546c7b1479bd1c
Nigel Green <matrixg45@gmail.com>
2020-12-02 10:18:13 -0500

    Completed Header/Navbar

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/* window.onload = function () {
+  var header = document.getElementById("header");
+  header.style.backgroundImage = "linear-gradient( 109.6deg,  rgba(44,83,131,1) 18.9%, rgba(95,175,201,1) 91.1% )";
+  header.style.transition = "2s";
+};
+*/
